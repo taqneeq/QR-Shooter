a5709991fe47dc87f3b431a7e25c6f1db5f07c55
vedantheda <vedant.heda@outlook.com>
2024-01-21 03:01:18 +0530

    global variables
    global variables to be edited later

---
+++ src/data/rewards.js
@@ -0,0 +1,14 @@
+const rewards = [
+    {
+      id: 1,
+      name: "Reward 1",
+      points: 100,
+    },
+    {
+      id: 2,
+      name: "Reward 2",
+      points: 200,
+    },
+  ];
+  
+  export default rewards;
