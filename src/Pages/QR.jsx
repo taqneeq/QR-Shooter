d7e8d0b670e16324b0b7f82f3f2e5e942cdc14e9
vedantheda <vedant.heda@outlook.com>
2024-01-21 01:57:43 +0530

    QR Logic
    QR Shooter logic implemented using Firebase.

--- src/Pages/QR.jsx
+++ src/Pages/QR.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { UserAuth } from '../context/AuthContext';
 import QRCode from 'react-qr-code';
-import { Link } from 'react-router-dom';
-import { BsArrowLeft } from 'react-icons/bs';
 import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 const db = getFirestore();
@@ -18,7 +16,8 @@ const QR = () => {
         const docSnap = await getDoc(userRef);
 
         if (docSnap.exists()) {
-          setPoints(docSnap.data().points || 0);
+          const userData = docSnap.data();
+          setPoints(userData.points || 0);
         }
       }
     };
@@ -28,7 +27,7 @@ const QR = () => {
 
   return (
     <div className="bg-tq-base min-h-screen flex rounded-3xl">
-      <div className=" bg-white h-fit rounded-3xl m-auto text-lg flex flex-col justify-center items-center p-3">
+      <div className="bg-white h-fit rounded-3xl m-auto text-lg flex flex-col justify-center items-center p-3">
         {user.uid ? (
           <div className="flex flex-col items-center">
             <h1 className="text-3xl text-tq-text mb-4">
@@ -36,11 +35,11 @@ const QR = () => {
             </h1>
             <div className="shadow-tq-surface rounded-lg shadow-md p-4 mb-8">
               <QRCode
-                value={user.uid ? user.uid : 'Loading..Please try again'}
+                value={user.uid ? user.uid : 'Loading... Please try again'}
               />
             </div>
             <p className="text-tq-text mt-2">
-              Your total points: {user.points || 0}
+              Your total points: {points}
             </p>
           </div>
         ) : (
